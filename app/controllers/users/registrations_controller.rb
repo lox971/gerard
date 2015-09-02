1ee081cd02b3bb75fbf91849ed172122d23d5a2e
julien jacquemond <julien.jacquemond@wanadoo.fr>
2015-09-02 16:35:32 +0200

    home

---
+++ app/controllers/users/registrations_controller.rb
@@ -0,0 +1,10 @@
+class RegistrationsController < Devise::RegistrationsController
+  protected
+
+  def after_sign_up_path_for(resource)
+    if resource.profile_type == "mover"
+      new_mover_path
+    else
+      new_customer_path
+  end
+end
