2ff7e934066c162aa8d89b851c4837e0dd013624
julien jacquemond <julien.jacquemond@wanadoo.fr>
2015-09-03 16:22:08 +0200

    modified logged navbar

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150901162707) do
+ActiveRecord::Schema.define(version: 20150903141817) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,8 @@ ActiveRecord::Schema.define(version: 20150901162707) do
     t.integer  "mover_rating"
     t.datetime "created_at",        null: false
     t.datetime "updated_at",        null: false
+    t.float    "latitude"
+    t.float    "longitude"
   end
 
   add_index "courses", ["customer_id"], name: "index_courses_on_customer_id", using: :btree
