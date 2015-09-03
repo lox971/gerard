373dfd251c803df1a794af32a365c83eae0a1d15
julien jacquemond <julien.jacquemond@wanadoo.fr>
2015-09-03 10:58:14 +0200

    home updated

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,10 +1,10 @@
 class Users::SessionsController < Devise::SessionsController
   protected
   def after_sign_in_path_for(resource)
-    if resource.profile_type == "mover"
-      new_mover_path
-    elsif resource.profile_type == "customer"
-      new_customer_path
+    if resource.profile_type == "Mover"
+      movers_path
+    elsif resource.profile_type == "Customer"
+      customers_path
     else
       request.env['omniauth.origin'] || stored_location_for(resource) || root_path
     end
