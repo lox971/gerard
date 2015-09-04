621790273f13a27394364767cbd74cf52498b8e0
julien jacquemond <julien.jacquemond@wanadoo.fr>
2015-09-04 12:09:01 +0200

    config model

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,5 +1,6 @@
 class Customer < ActiveRecord::Base
   has_one :user, as: :profile
+  has_many :courses
 
   # validates :first_name, presence: true
   # validates :last_name, presence: true
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150903154941) do
+ActiveRecord::Schema.define(version: 20150904095618) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -56,6 +56,8 @@ ActiveRecord::Schema.define(version: 20150903154941) do
     t.integer  "course_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.float    "latitude"
+    t.float    "longitude"
   end
 
   add_index "sites", ["course_id"], name: "index_sites_on_course_id", using: :btree
