26e04344557a256f2479b74c8c85913a7152828f
Hadrien Pirmez <hadrien.pirmez@gmail.com>
2015-09-05 13:17:40 +0200

    install gem pundit and add policies in application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,9 +5,21 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  include Pundit
+
+  after_action :verify_authorized, except: :index, unless: :devise_controller?
+  after_action :verify_policy_scoped, only: :index, unless: :devise_controller?
+
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
 
   protected
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:sign_up) << :profile_type
   end
+
+  def user_not_authorized
+    flash[:alert] = "You are not authorized to perform this action."
+    redirect_to(root_path)
+  end
 end
+
