26d5f0d957e72f7f3d866991a9e77022f25d6157
Hadrien Pirmez <hadrien.pirmez@gmail.com>
2015-09-05 13:04:23 +0200

    add user admin interface

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150905100452) do
+ActiveRecord::Schema.define(version: 20150905104246) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -95,18 +95,18 @@ ActiveRecord::Schema.define(version: 20150905100452) do
   add_index "trucks", ["mover_id"], name: "index_trucks_on_mover_id", using: :btree
 
   create_table "users", force: :cascade do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
+    t.string   "email",                  default: "",    null: false
+    t.string   "encrypted_password",     default: "",    null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
+    t.integer  "sign_in_count",          default: 0,     null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.inet     "current_sign_in_ip"
     t.inet     "last_sign_in_ip"
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
+    t.datetime "created_at",                             null: false
+    t.datetime "updated_at",                             null: false
     t.string   "provider"
     t.string   "uid"
     t.string   "picture"
@@ -114,6 +114,7 @@ ActiveRecord::Schema.define(version: 20150905100452) do
     t.datetime "token_expiry"
     t.integer  "profile_id"
     t.string   "profile_type"
+    t.boolean  "admin",                  default: false, null: false
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
