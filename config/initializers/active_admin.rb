b4363482f42848659e704d6153067ad02d1c73f7
Hadrien Pirmez <hadrien.pirmez@gmail.com>
2015-09-05 12:04:33 +0200

    change in the active admin file

--- config/initializers/active_admin.rb
+++ config/initializers/active_admin.rb
@@ -1,3 +1,7 @@
+def authenticate_admin!
+  redirect_to new_user_session_path unless current_user.admin
+end
+
 ActiveAdmin.setup do |config|
   # == Site Title
   #
@@ -54,7 +58,7 @@ ActiveAdmin.setup do |config|
   #
   # This setting changes the method which Active Admin calls
   # within the application controller.
-  # config.authentication_method = :authenticate_admin_user!
+  config.authentication_method = :authenticate_admin_user!
 
   # == User Authorization
   #
@@ -86,7 +90,7 @@ ActiveAdmin.setup do |config|
   #
   # This setting changes the method which Active Admin calls
   # (within the application controller) to return the currently logged in user.
-  # config.current_user_method = :current_admin_user
+  config.current_user_method = :current_admin_user
 
   # == Logging Out
   #
@@ -104,7 +108,7 @@ ActiveAdmin.setup do |config|
   # link. For example :get, :delete, :put, etc..
   #
   # Default:
-  # config.logout_link_method = :get
+  config.logout_link_method = :get
 
   # == Root
   #
