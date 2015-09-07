a730f0b23892ce3b3a45178d15bf6b0afda0a821
Hadrien Pirmez <hadrien.pirmez@gmail.com>
2015-09-07 12:43:01 +0200

    change views to upload picture customer

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -4,4 +4,10 @@ class Customer < ActiveRecord::Base
 
   # validates :first_name, presence: true
   # validates :last_name, presence: true
+
+  has_attached_file :picture,
+    styles: { medium: "300x300>", thumb: "100x100>" }
+
+  validates_attachment_content_type :picture,
+    content_type: /\Aimage\/.*\z/
 end
