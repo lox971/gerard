45cd53a4ec988cda0f51009fc127c3438ff0aa7a
Hadrien Pirmez <hadrien.pirmez@gmail.com>
2015-09-07 11:44:48 +0200

    add users trucks movers courses to dashboard

---
+++ app/admin/truck.rb
@@ -0,0 +1,18 @@
+ActiveAdmin.register Truck do
+
+# See permitted parameters documentation:
+# https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+#
+# permit_params :list, :of, :attributes, :on, :model
+#
+# or
+#
+# permit_params do
+#   permitted = [:permitted, :attributes]
+#   permitted << :other if resource.something?
+#   permitted
+# end
+
+  permit_params :mover_id, :brand, :model, :year, :registration, :truck_identification, :created_at, :updated_at
+
+end
