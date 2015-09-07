73494de11939aa50c772d4f84867190c86f56dbf
julien jacquemond <julien.jacquemond@wanadoo.fr>
2015-09-07 15:05:12 +0200

    link ok

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -3,7 +3,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
   protected
 
   def after_sign_up_path_for(resource)
-    if resource.profile_type == "mover" || "Mover"
+    if resource.profile_type == "mover" || resource.profile_type == "Mover"
       new_mover_path
     else
       new_customer_path
