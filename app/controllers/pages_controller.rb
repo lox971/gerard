ecff553af450370af590dfb6a28224db9b432b7a
Hadrien Pirmez <hadrien.pirmez@gmail.com>
2015-09-07 17:07:56 +0200

    creation of routes for estimation and change view of home

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,13 +1,40 @@
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: :home
 
-  layout 'static'
-
   def home
+    @course = Course.new
+    @course.sites.build
     if current_user && current_user.provider == "facebook"
       redirect_to edit_customer_path(current_user)
     else
     end
   end
 
+  private
+
+  def course_params
+    params.require(:course).permit(:mover_rating)
+  end
+
+
+  def address
+    params[:course][:sites_attributes]["0"].each do |key, value|
+      if "address" == key
+        @site.address = value
+      else
+        @site.type_of = value
+      end
+    end
+  end
+
+  def type
+    params[:course][:sites_attributes]["1"].each do |key, value|
+      if "address" == key
+        @site.address = value
+      else
+        @site.type_of = value
+      end
+    end
+  end
 end
+
