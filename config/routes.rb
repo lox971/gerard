8395f61edbdfb91c0cee63c68bc99012c72ea7e4
loic geoffroy <loic@opitrip.Com>
2015-09-08 17:54:55 +0200

    home page

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   ActiveAdmin.routes(self)
   devise_for :users, controllers: { omniauth_callbacks: "users/omniauth_callbacks", registrations: "users/registrations", sessions: "users/sessions"}
 
-  root to: 'courses#new'
+  root to: 'pages#home'
 
   resources :customers
   resources :courses do
