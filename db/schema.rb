e4bf7e8c03c0830ed908c0c05c82a4064036fb31
julien jacquemond <julien.jacquemond@wanadoo.fr>
2015-09-10 19:08:10 +0200

    signup

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 20150910120219) do
-
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -48,13 +46,12 @@ ActiveRecord::Schema.define(version: 20150910120219) do
     t.integer  "price_cents",          default: 0, null: false
     t.float    "kms"
     t.integer  "time"
+    t.string   "status"
+    t.json     "payment"
     t.string   "picture_file_name"
     t.string   "picture_content_type"
     t.integer  "picture_file_size"
     t.datetime "picture_updated_at"
-    t.string   "status"
-    t.json     "payment"
-
   end
 
   add_index "courses", ["customer_id"], name: "index_courses_on_customer_id", using: :btree
